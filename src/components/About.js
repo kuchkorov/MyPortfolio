4a2923efc954c6e95aac371599fc6b01a6d1d291
Mamurjon <kuchkorov.mamurjon@mail.ru>
2021-09-27 12:12:36 +0500

    install animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./About.css";
 import Avatar from "../img/avatar.svg";
+import "aos/dist/aos.css";
+import AOS from 'aos';
 
 const About = () => {
+
+    useEffect(() => {
+        AOS.init();
+    });
+
     return (
         <>
             <div id="about" className="about-me my-5">
                 <div className="container">
-                    <h1 className="text-center">About Me</h1>
+                    <h1 className="text-center" data-aos="fade-top" data-aos-duration="1500">About Me</h1>
                     <div className="row">
-                        <div className="col-md-6 img-avatar">
+                        <div className="col-md-6 img-avatar" data-aos="fade-right" data-aos-duration="1500">
                             <img src={ Avatar } className=""  />
                         </div>
-                        <div className="col-md-6 about-text">
+                        <div className="col-md-6 about-text" data-aos="fade-left" data-aos-duration="1500">
                             <h1>I am Frontend Developer</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, quasi fugiat? Culpa autem fugiat vel atque a est, 
                                 repellat dolorem. Ea minima velit eveniet? Assumenda consequatur quia culpa impedit hic, modi obcaecati eum nostrum! 
